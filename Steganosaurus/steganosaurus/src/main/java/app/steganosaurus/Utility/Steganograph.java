f7102eec756edae3390c6f5b17a632cbfb31b172
gbastien1 <gabrielle.bastien1@gmail.com>
2016-03-27 23:06:36 -0400

    More work on hiding text in bytes

--- Steganosaurus/steganosaurus/src/main/java/app/steganosaurus/Utility/Steganograph.java
+++ Steganosaurus/steganosaurus/src/main/java/app/steganosaurus/Utility/Steganograph.java
@@ -41,19 +41,108 @@ public class Steganograph {
         return picture;
     }
 
+    //Translates the image into a string and encode the string into the given image
     private Pixel[] encodePixels(Pixel[] destPixels, Pixel[] hidePixels){
         Random rand = new Random();
         int min = 0;
-        int max = 255;
+        int max = 0;
 
         for (Pixel P:destPixels) {
-            P.R = rand.nextInt((max - min) + 1) + min;
-            P.G = rand.nextInt((max - min) + 1) + min;
-            P.B = rand.nextInt((max - min) + 1) + min;
+            P.R += rand.nextInt((max - min) + 1) + min;
+            P.G += rand.nextInt((max - min) + 1) + min;
+            P.B += rand.nextInt((max - min) + 1) + min;
+
+            P.R = Math.min(255, Math.max(0, P.R));
+            P.G = Math.min(255, Math.max(0, P.G));
+            P.B = Math.min(255, Math.max(0, P.B));
+
+        }
+
+        /*
+        String testMessage = "";
+        for(int i =0; i<10000; i++) {
+            testMessage += '0';
+        }
+        hideMessageInPixels(destPixels,7, testMessage);
+        */
+
+        return destPixels;
+    }
+
+    //Encode the given string into the destination pixels
+    private Pixel[] encodeString(Pixel[] destPixels, String rawData){
+        Random rand = new Random();
+        int min = -30;
+        int max = 30;
+
+
+        for (Pixel P:destPixels) {
+            P.R += rand.nextInt((max - min) + 1) + min;
+            P.G += rand.nextInt((max - min) + 1) + min;
+            P.B += rand.nextInt((max - min) + 1) + min;
+
+            P.R = Math.min(255, Math.max(0, P.R));
+            P.G = Math.min(255, Math.max(0, P.G));
+            P.B = Math.min(255, Math.max(0, P.B));
 
         }
 
         return destPixels;
+    }
+
+    private void hideMessageInPixels(Pixel[] destPixels,  int bitPerColor, String message) {
+        int curBit = 0;
+        int curPixel = 0;
+        int curColor = 0; //0=red, 1=green, 2=blue
+
+        for(int i =0; i<message.length(); i++){
+            switch(curColor){
+                case 0 :
+                    if(message.charAt(i) == '1') destPixels[curPixel].R = (byte) (destPixels[curPixel].R | (1 << (7-curBit)));
+                    else  destPixels[curPixel].R = (byte) (destPixels[curPixel].R & ~(1 << (7-curBit)));
+                    break;
+                case 1 :
+                    if(message.charAt(i) == '1') destPixels[curPixel].G = (byte) (destPixels[curPixel].G | (1 << (7-curBit)));
+                    else  destPixels[curPixel].G = (byte) (destPixels[curPixel].G & ~(1 << (7-curBit)));
+                    break;
+                case 2 :
+                    if(message.charAt(i) == '1') destPixels[curPixel].B = (byte) (destPixels[curPixel].B | (1 << (7-curBit)));
+                    else  destPixels[curPixel].B = (byte) (destPixels[curPixel].B & ~(1 << (7-curBit)));
+                    break;
+            }
+
+            curBit++;
+            if(curBit >= bitPerColor) {
+                curBit = 0;
+                curColor++;
+                if(curColor >= 3){
+                    curPixel++;
+                    curColor = 0;
+                    if(curPixel >= destPixels.length){
+                        i = message.length(); //Stop
+                    }
+                }
+            }
+        }
+    }
+
+    // Convert pixel into a string
+    private String getPixelData(Pixel pixel){
+        String pData = "";
+        //Red
+        if(pixel.R < 10) pData += ("00" + pixel.R);
+        else if(pixel.R < 100) pData += ("0" + pixel.R);
+        else pData += pixel.R;
+        //Green
+        if(pixel.G < 10) pData += ("00" + pixel.G);
+        else if(pixel.G < 100) pData += ("0" + pixel.G);
+        else pData += pixel.G;
+        //Blue
+        if(pixel.B < 10) pData += ("00" + pixel.B);
+        else if(pixel.B < 100) pData += ("0" + pixel.B);
+        else pData += pixel.B;
+
+        return pData;
     }
 
     private Pixel[] getRGBFromBitmap(Bitmap receivedPicture){
